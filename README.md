d57606f89d7d89b6c3583ebe26a8b5f7a9c2491e
Mridul Seth <seth.mridul@gmail.com>
2021-06-04 00:29:11 +0200

    revamp fomo

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### twitterFOMO
+## twitterFOMO
 
 Don't want to spend hours scrolling away on Twitter but also feel that your feed
 is important to keep up with updates (fear of missing out).
@@ -19,13 +19,13 @@ tweet text - user_name - link_to_tweet
 
 #### Quick Setup
 
-The scripts works with Python 3, and assumes you have a server/computer which takes care of running the cron jobs.
+The scripts works with Python 3, and assumes you have a server/computer which takes care of running the python script.
 
 You would need API keys from Twitter(to get tweets) and Sendgrid(to send the email) for authentication.
 
-Create a twitter dev account -> https://developer.twitter.com/en/application/use-case
+Create a twitter dev account: https://developer.twitter.com/en/application/use-case
 
-Sendgrid setup -> https://sendgrid.com/docs/for-developers/sending-email/api-getting-started/
+Sendgrid: https://sendgrid.com/docs/for-developers/sending-email/api-getting-started/
 
 Clone the repo on your server/computer, create a directory to hold the data and install requirements.
 
@@ -43,27 +43,19 @@ export SENDGRID_API_KEY='YOUR_API_KEY'
 export TWITTER_CONSUMER_KEY='TWITTER_CONSUMER_KEY_FROM_YOUR_TWITTER_APP'
 export TWITTER_CONSUMER_SECRET='TWITTER_CONSUMER_SECRET_FROM_YOUR_TWITTER_APP'
 export TWITTER_TOKEN='TWITTER_TOKEN_FROM_YOUR_TWITTER_APP'
-export TWITTER_TOKEN_SECRET ='TWITTER_TOKEN_SECRET_FROM_YOUR_TWITTER_APP'
+export TWITTER_TOKEN_SECRET='TWITTER_TOKEN_SECRET_FROM_YOUR_TWITTER_APP'
 export FROM_EMAIL="YOUR_EMAIL"
 export TO_EMAIL="TO_EMAIL"
+export FETCH=6
 ```
 Run the config file to load in your API keys.
 ```
 source ./config.env
 ```
-Setup cron jobs using `crontab -e` and add the following to the file
-```
-1 0 * * * Path_to/python3 Path_to/twitterFOMO/send_email.py >> Path_to/cron.log 2>&1
-0 1,7,13,19 * * * Path_to/python3 Path_to/twitterFOMO/get_twitter_timeline.py >> Path_to/cron.log 2>&1
-```
 
-Note: if using virtual environments you need to take care of `Path_to/python3` in the cron jobs.
+[Email at 01:00 server time, and fetch twitter updates every 6 hours. Change it to hourly (`FETCH=1`) if you follow a LOT of people, as the API restricts to last 200 tweets on your homepage]
 
-[Email at 00:01 server time, and fetch twitter updates at 01:00, 07:00, 13:00, 19:00. Make it hourly if you follow a LOT of people, as the API restricts to last 200 tweets on your homepage]
 
-##### Troubleshoot
-
-Quick Hack: To make sure the cron jobs are working change the time to current time + 5 mins for the `get_twitter_timeline.py` file and current time + 10 mins for `send_email.py` in `crontab` . If you don't get an email, first check spam folder and then check the `cron.log` file to see what went wrong.
 
 
 
---
+++ twitter_fomo.py
@@ -0,0 +1,106 @@
+import ast
+import logging
+import os
+import time
+from datetime import datetime, timedelta
+from pathlib import Path
+
+import pandas as pd
+import schedule
+import tweepy
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+logging.basicConfig(
+    filename="logs.txt",
+    filemode="a",
+    format="%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s",
+    datefmt="%H:%M:%S",
+    level=logging.DEBUG,
+)
+
+
+def get_twitter_timeline(
+    consumer_key, consumer_secret, access_token, access_token_secret
+):
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    api = tweepy.API(auth)
+    logging.debug("Fetching home timeline.")
+    public_tweets = api.home_timeline(count=200, tweet_mode="extended")
+
+    df = pd.DataFrame.from_records([tweet._json for tweet in public_tweets])
+    df.created_at = pd.to_datetime(df.created_at)
+    df["date"] = df.created_at.apply(lambda x: x.date())
+    df = df[["id", "user", "full_text", "date"]]
+    dates = df.date.unique()
+
+    for i in dates:
+        my_file = Path("data/" + "tweets_" + str(i) + ".csv")
+        if my_file.is_file():
+            temp_read = pd.read_csv(my_file)
+            new_dump = pd.concat([temp_read, df[df.date == i]]).drop_duplicates(
+                subset="id"
+            )
+            new_dump.to_csv(my_file, index=False)
+        else:
+            logging.debug(f"Creating file {my_file}")
+            df[df.date == i].to_csv(my_file, index=False)
+
+
+def send_email(SENDGRID_API_KEY, from_email, to_emails):
+    email_date = str((datetime.today() - timedelta(days=1)).date())
+    filename = "data/" + "tweets_" + email_date + ".csv"
+    data = pd.read_csv(filename)
+
+    logging.debug(f"Creating content for {email_date}")
+    content = list()
+    for tweet in data.itertuples():
+        text = tweet.full_text
+        user = ast.literal_eval(tweet.user)["name"]
+        link = "https://twitter.com/i/web/status/" + str(tweet.id)
+        content.append(str("<li>" + text + " - " + user + " - " + link + "</li>"))
+
+    content = " \n ".join(content)
+
+    message = Mail(
+        from_email=from_email,
+        to_emails=to_emails,
+        subject="Twitter Update for " + email_date,
+        html_content="<ul>" + content + "</ul>",
+    )
+    try:
+        sg = SendGridAPIClient(SENDGRID_API_KEY)
+        response = sg.send(message)
+        logging.debug(response.status_code)
+        logging.debug(response.body)
+        logging.debug(response.headers)
+    except Exception as e:
+        logging.debug(e.message)
+
+
+if __name__ == "__main__":
+    consumer_key = os.environ.get("TWITTER_CONSUMER_KEY")
+    consumer_secret = os.environ.get("TWITTER_CONSUMER_SECRET")
+    access_token = os.environ.get("TWITTER_TOKEN")
+    access_token_secret = os.environ.get("TWITTER_TOKEN_SECRET")
+    SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
+    from_email = os.environ.get("FROM_EMAIL")
+    to_emails = os.environ.get("TO_EMAIL")
+    fetch = os.environ.get("FETCH")
+    # get_twitter_timeline(consumer_key, consumer_secret, access_token, access_token_secret)
+    # send_email(SENDGRID_API_KEY, from_email, to_emails)
+    def _fetch_tweet_job():
+        get_twitter_timeline(
+            consumer_key, consumer_secret, access_token, access_token_secret
+        )
+
+    def _send_email():
+        send_email(SENDGRID_API_KEY, from_email, to_emails)
+
+    schedule.every(fetch).hour.do(_fetch_tweet_job)
+    schedule.every().day.at("01:00").do(_send_email)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
